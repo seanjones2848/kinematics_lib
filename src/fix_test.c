9b2741a2c0fc0b620235f91b5698ec2c77e73b5a
Sean Jones <seanjones2848@gmail.com>
2023-06-16 13:42:54 -0700

    cleaning up tests

--- src/fix_test.c
+++ src/fix_test.c
@@ -6,12 +6,11 @@
 
 int main()
 {
-    printf("-1.8-3 = %s\n", fix_cstr(FIX_SUB(FIX_CONST(-1.8), FIX_FROM_INT(3)), -2));
-    printf("1-2 = %s\n", fix_cstr(FIX_SUB(FIX_CONST(1), FIX_CONST(2)), -2));
     printf("pi = %s\n", fix_cstr(FIX_PI, -2));
     printf("one = %s\n", fix_cstr(FIX_ONE, -2));
     printf("e = %s\n", fix_cstr(FIX_E, -2));
     printf("2.5+3 = %s\n", fix_cstr(FIX_ADD(FIX_CONST(2.5), FIX_FROM_INT(3)), -2));
+    printf("-1.8-3 = %s\n", fix_cstr(FIX_SUB(FIX_CONST(-1.8), FIX_FROM_INT(3)), -2));
     printf("2.5*3 = %s\n", fix_cstr(FIX_MUL(FIX_CONST(2.5), FIX_FROM_INT(3)), -2));
     printf("1/4 = %s\n", fix_cstr(FIX_DIV(FIX_FROM_INT(1), FIX_FROM_INT(4)), -2));
     printf("exp(1) = %s\n", fix_cstr(fix_exp(FIX_ONE), -2));
